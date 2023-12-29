155b73f7db6a53c56f52f5ffd1eaa5b4a73a840f
Oleg Shpagin <info@wiseplat.org>
2023-12-29 18:52:04 +0300

    up

---
+++ utils_get_tickers_from_securities.py
@@ -0,0 +1,26 @@
+# git clone https://github.com/cia76/FinamPy
+# pip install pytz, requests, grpcio, protobuf, types-protobuf, googleapis-common-protos
+
+import os, ast
+from FinamPy.grpc.tradeapi.v1.securities_pb2 import GetSecuritiesResult  # Тип списка инструментов
+from google.protobuf.json_format import MessageToJson, Parse  # Будем хранить справочник в файле в формате JSON
+
+
+if __name__ == '__main__':
+    filename = os.path.join("cached", f"finam_securities.txt")
+    securities = GetSecuritiesResult()  # Тип списка инструментов
+
+    with open(filename, 'r', encoding='UTF-8') as f:  # Открываем файл на чтение
+        securities = MessageToJson(Parse(f.read(), securities))  # Получаем список инструментов из файла, приводим к типу
+
+    securities = ast.literal_eval(securities)  # превращаем в словарь
+
+    # приводим к словарю по инструментам
+    all_securities = {}
+    all_securities_usa = {}
+    for sec_info in securities["securities"]:
+        all_securities[sec_info["code"]] = sec_info
+        if sec_info['board'] == "MCT": all_securities_usa[sec_info["code"]] = sec_info
+
+    # print(all_securities)
+    print(all_securities_usa)
